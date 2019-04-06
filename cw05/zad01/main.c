16f1cca54da177233ab8a1058c3368c92695178c
RetroSuperHero <eryksikora9804@gmail.com>
2019-04-07 01:09:27 +0200

    cw05

---
+++ cw05/zad01/main.c
@@ -0,0 +1,131 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <wait.h>
+
+int maxCommands = 10;
+
+char** getCommandElements(char *line){
+    int noOfArguments = 1;
+    char* lineCpy = calloc(1000, sizeof(char));
+    strcpy(lineCpy, line);
+    char tokens[3] = {' ','\n','\t'};
+
+    strtok(lineCpy, tokens);
+    while(strtok(NULL, tokens)) {
+        noOfArguments++;
+    }
+
+    char** arguments = calloc(noOfArguments+1, sizeof(char*));
+    arguments[0] = strtok(line, tokens);
+
+    for(int i=1; i<noOfArguments; ++i) {
+        arguments[i] = strtok(NULL, tokens);
+    }
+
+    return arguments;
+}
+
+void execLine(char* line) {
+    int commandsNumber = 1;
+    int pipes[2][2];
+    char *commands[maxCommands];
+
+    char* lineCpy = calloc(strlen(line), sizeof(char));
+    strcpy(lineCpy, line);
+
+    strtok(lineCpy, "|");
+    while(strtok(NULL, "|")) {
+        commandsNumber++;
+    }
+
+    free(lineCpy);
+
+    commands[0] = strtok(line, "|");
+    for(int i=1; i<commandsNumber; ++i) {
+        commands[i] = strtok(NULL, "|");
+    }
+
+    for (int i=0; i<commandsNumber; ++i) {
+        if (i>0) {
+            close(pipes[i % 2][0]);
+            close(pipes[i % 2][1]);
+        }
+
+        if(pipe(pipes[i % 2]) == -1) {
+            printf("Błąd potoku\n");
+            exit(-1);
+        }
+
+        if (fork() == 0) {
+            char** commandElements = getCommandElements(commands[i]);
+
+            if (i != commandsNumber-1) {
+                close(pipes[i % 2][0]);
+                if (dup2(pipes[i % 2][1], STDOUT_FILENO) < 0) {
+                    exit(-1);
+                };
+            }
+            if (i != 0) {
+                close(pipes[(i + 1) % 2][1]);
+                if (dup2(pipes[(i + 1) % 2][0], STDIN_FILENO) < 0) {
+                    close(-1);
+                }
+            }
+            execvp(commandElements[0], commandElements);
+            free(commandElements);
+
+            exit(0);
+        }
+    }
+    close(pipes[commandsNumber % 2][0]);
+    close(pipes[commandsNumber % 2][1]);
+    for(int i=0; i<commandsNumber; ++i) {
+        wait(NULL);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        printf("Podano za małą liczbę argumentów.\n");
+    } else {
+        char* listaPath = argv[1];
+        FILE* lista = fopen(listaPath, "r");
+
+        if(lista) {
+            fseek(lista, 0L, SEEK_END);
+            int size = ftell(lista);
+            fseek(lista, 0L, SEEK_SET);
+
+            char* buff = calloc(size, sizeof(char));
+            char* original = calloc(size, sizeof(char));
+
+            if(fread(buff, sizeof(char), size, lista)) {
+                strcpy(original, buff);
+                int noOfLines = 1;
+                strtok(buff, "\n");
+
+                while(strtok(NULL, "\n")) {
+                    noOfLines++;
+                }
+
+                char* lines[noOfLines];
+                char* first = strtok(original, "\n");
+                printf("\n");
+
+                for(int i=0; i<noOfLines; ++i) {
+                    lines[i] = first;
+                    first = strtok(NULL, "\n");
+                }
+                
+                for(int i=0; i<noOfLines; ++i) {
+                    execLine(lines[i]);
+                }
+            }
+            free(buff);
+            free(original);
+        }
+    }
+}
